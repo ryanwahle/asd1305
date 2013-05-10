03fd7501dde33d75bd8a42227d9314a277930313
Ryan Wahle <ryanwahle@fullsail.edu>
2013-05-09 19:28:58 -0700

    shell for week2 development

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 /* Ryan Wahle */
 /* ASD 1305   */
+
+$("#pageMain").on("pageinit", function() {
+	// See if there is data in localStorage.
+	// If not, then ask user which data they would like to load.
+	// Display TV Shows == today && >= whatTimeIsItNow
+});
+
+$("#pageAddEditShow").on("pageinit", function() {
+	// See if there is a key associated with this click.
+	// If there is a key, then load show data and change form to represent edit form
+	// If no key, then change form to represent add form
+});
